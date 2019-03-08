282d4ee931f051ee86743ec7387d688ae0212919
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-03-07 23:53:45 -0500

    add api controllers

---
+++ app/controllers/api/v1/shortener_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'lib/clients/bitly_client'
+
+module Api
+  module V1
+    class ShortenerController < ApplicationController
+      def create
+      end
+    end
+  end
+end
